06ec5d65193f76decc1c5953e60eda95f43f04b8
Matheus de Lima Santana <matheus.lima@take.net>
2023-08-30 21:42:16 -0300

    Create procedure to insert new votes

---
+++ src/VotingSystemBigBrotherBrasil.Consumer.Data/Migrations/20230831002515_sp-IncrementVote.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VotingSystemBigBrotherBrasil.Consumer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class spIncrementVote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[InsertVote]
+                    @ParticipantName varchar(50)
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+                    DECLARE @vote_count INT;
+
+                    SET @vote_count = (
+                        SELECT 
+                                Votes
+                        FROM
+                                Votes
+                    ) + 1
+                
+                    UPDATE Votes SET Votes = @vote_count WHERE ParticipantName = @ParticipantName
+                END";
+
+            migrationBuilder.Sql(sp);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
