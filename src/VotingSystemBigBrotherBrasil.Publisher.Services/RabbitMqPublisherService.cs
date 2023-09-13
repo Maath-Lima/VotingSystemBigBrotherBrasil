9277acf751d23ada0377d557a90a6074e22a7dd3
Matheus de Lima Santana <math3226@gmail.com>
2023-09-12 21:11:08 -0300

    Fair Dispatch

--- src/VotingSystemBigBrotherBrasil.Publisher.Services/RabbitMqPublisherService.cs
+++ src/VotingSystemBigBrotherBrasil.Publisher.Services/RabbitMqPublisherService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 using System.Text;
+using System.Threading.Channels;
 using VotingSystemBigBrotherBrasil.Common;
 using VotingSystemBigBrotherBrasil.Publisher.Models.Settings;
 
@@ -27,6 +28,8 @@ namespace VotingSystemBigBrotherBrasil.Publisher.Services
         public void PublishVote(string name)
         {
             _channel.QueueDeclare(RabbitMqConstants.QUEUE_NAME, RabbitMqConstants.QUEUE_DURABLE, RabbitMqConstants.QUEUE_EXCLUSIVE, RabbitMqConstants.QUEUE_AUTO_DELETE);
+
+            _channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
             _channel.BasicPublish(string.Empty, RabbitMqConstants.QUEUE_NAME, ConfigQueueProperties(), Encoding.UTF8.GetBytes(name));
         }
